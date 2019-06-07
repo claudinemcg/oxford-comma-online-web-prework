4231300766be000175f348d422ce2d14981b7503
Claudine McGowan <claudine.mcgowan@gmail.com>
2019-06-07 10:27:48 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,8 @@
 def oxford_comma(array)
-  
+  new_array = []
+  new_array << array.pop
+  first_part = array.join(', ')
+  second_part = ", and #{new_array.join}"
+  puts "#{first_part}#{second_part}"
+
 end
