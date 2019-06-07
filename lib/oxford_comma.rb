2f4e6ae38c1667ed61d1d4857e38c9065138c3be
Claudine McGowan <claudine.mcgowan@gmail.com>
2019-06-07 10:46:29 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,15 +1,15 @@
 def oxford_comma(array)
-  if array.length >2
+  if array.length >2                        # yes, no, and maybe
     new_array = []
     new_array << array.pop
     first_part = array.join(', ')
     second_part = ", and #{new_array.join}"
     return "#{first_part}#{second_part}"
   
-  elsif array.length == 2
+  elsif array.length == 2                   #yes and no
     array.insert(1, "and")
     return array.join(" ")
     
-  else return array.join
+  else return array.join                    
   end
 end
